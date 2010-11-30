e445335d92fe5d4a076bf63f6a49a593d876f18b
Edward O'Connor <ted@oconnor.cx>
2010-11-30 13:55:37 +0100

    update

--- xml-stream.el
+++ xml-stream.el
@@ -4,6 +4,7 @@
 
 ;; Author: Edward O'Connor <ted@oconnor.cx>
 ;; Keywords: comm, tools, processes
+;; Version: 1.0.0
 
 ;; This file is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the
